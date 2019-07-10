441dbd08d9def2eb4809cfa453fd7f95576cc69e
yahya <24tech.tj@gmail.com>
2019-07-10 15:26:35 +0500

    Added tests

---
+++ app/src/test/java/ru/skillbranch/devintensive/BaseMessageTest.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive
+
+import org.junit.Assert.*
+import org.junit.Test
+import ru.skillbranch.devintensive.models.*
+import java.util.*
+
+class BaseMessageTest {
+
+    @Test
+    fun makeMessageTest(){
+        val user = User.makeUser("Иван Кузьмин")
+        var message = BaseMessage.makeMessage(user, Chat("0"), Date(), "text", "any text message")
+        assertTrue(message is TextMessage)
+        assertEquals("0", message.id)
+        assertTrue(message.from === user)
+        message = BaseMessage.makeMessage(user, Chat("0"), Date(), "image", "https://anyurl.com")
+        assertTrue(message is ImageMessage)
+        assertEquals("1", message.id)
+       // assertTrue(message.isIncoming)
+        assertEquals((message as ImageMessage).image, "https://anyurl.com")
+
+    }
+}
